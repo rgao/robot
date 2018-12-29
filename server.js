046740fed6dfed9d40024da8991de5661eeb8cba
rgao <xinghoshi@gmail.com>
2018-12-28 17:30:15 -0800

    removed app folder and fixed most pathing issues for server.js

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require("express");
+const express = require("express");
+const path = require("path");
 
-var app = express();
+const db = require("./models");
+
 var PORT = process.env.PORT || 8080;
 
-var db = require("./app/models");
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("public"));
-
-var exphbs = require("express-handlebars");
-
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// var routes = require("./controllers/catsController.js");
-// app.use(routes);
+const routes = require("./controller/robot_controller.js");
+app.use(routes);
 
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
