0ee371b5f68162eaa99fc0ce086acb5be5d5bca3
rgao <xinghoshi@gmail.com>
2018-12-30 18:01:24 -0800

    implemented authentic authentication

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const path = require("path");
+const app = express();
+const logger = require("morgan");
+var debug = require("debug")("express-example");
+const session = require('express-session');
 
+const passport = require("./config/passport");
+const config = require("./config/extra-config");
 var db = require("./models");
 
-var PORT = process.env.PORT || 8080;
-
-const app = express();
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,10 +20,13 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// const isAuth = require("./config/middleware/authenticate");
-// const authCheck = require('./config/middleware/authenticationStatus');
-// app.use(isAuth);
-// app.use(authCheck);
+const isAuth = require("./config/middleware/authenticate");
+const authCheck = require('./config/middleware/authenticationStatus');
+
+app.use(session({ secret: config.sessionKey, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(authCheck);
 
 require("./routes")(app);
 
@@ -37,8 +44,11 @@ app.use(function (err, req, res, next) {
     })
 });
 
-db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
+app.set('port', process.env.PORT || 8080);
+
+db.sequelize.sync().then(function () {
+    var server = app.listen(app.get('port'), function () {
+        debug('Express server listening on port ' + server.address().port);
     });
 });
+
